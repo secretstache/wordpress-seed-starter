841490f95b0c630294d2410221e42af3e4ccb4ed
Dima Fedorenko <dev.fedorenko@gmail.com>
2024-07-24 12:49:38 +0300

    Init TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} config */
+const config = {
+    content: ['./app/**/*.php', './resources/**/*.{php,vue,js}'],
+    theme: {
+        extend: {
+            colors: {
+            }, // Extend Tailwind's default colors
+        },
+    },
+    plugins: [],
+};
+
+export default config;
